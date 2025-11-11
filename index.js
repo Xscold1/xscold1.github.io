8216e6fff336d3abe2542801556de62416cbd4fd
xscold1 <vashivan116@gmail.com>
2025-11-11 22:31:39 +0800

    Fix initial design of project and experience page and improve css file

--- index.js
+++ index.js
@@ -2,74 +2,84 @@ let currentPage = 0;
 const main = document.getElementById("main");
 
 function scrollToNext() {
-    const scrollAmount = window.scrollX + window.innerWidth;
-    window.scrollTo({
-        left: scrollAmount,
-        behavior: 'smooth'
-    });
+  const scrollAmount = window.scrollX + window.innerWidth;
+  window.scrollTo({
+    left: scrollAmount,
+    behavior: "smooth",
+  });
 }
 
 function scrollToPrev() {
-    const scrollAmount = window.scrollX - window.innerWidth;
-    window.scrollTo({
-        left: scrollAmount,
-        behavior: 'smooth'
-    });
+  const scrollAmount = window.scrollX - window.innerWidth;
+  window.scrollTo({
+    left: scrollAmount,
+    behavior: "smooth",
+  });
 }
 
 const experiences = {
   gcg: `
     <div class="expContainer">
-      <h1>Junior Software Engineer</h1>
-      <div class="keyRespCard">
-        <h1>Key Responsibilities</h1>
-        <ul>
-          <li>
-            Led the development of the <b>Human Resource Information System</b> improving the hiring process, HR workflow efficiency, and reduce dependency on manual spreadsheet and email-based tracking
-          </li>
-          <li>
-            Developed web applications using <b>Alpine JS</b> and <b>Typescript</b> for front-end. <b>PHP</b> with <b>Eloquent ORM</b> for back-end, and <b>Mysql</b> for the Database
-          </li>
-          <li>
-            Ensure clean and maintainable through the integration of PHP Stan for static analysis, and improved documentation practices
-          </li>
-          <li>
-            Collaborated on the development rebuilding of the  <b>agency's website</b> and <b>content management system</b>, contributing to both front-end and back-end features to meet project deliverables and deadlines
-          </li>
-        </ul>
+      <div class="expContainerHeader">
+        <h1>Junior Software Engineer</h1>
       </div>
-      <label>Skills</label>
-      <ul class="skillBadge">
-        <li>Alpine JS</li>
-        <li>PHP</li>
-        <li>Eloquent</li>
-        <li>MySQL</li>
-      </ul>
-    </div>
-  `,
-  da: `
-    <div class="expContainer">
-      <h1>Computer Programmer I</h1>
-      <div class="keyRespCard">
-        <h1>Key Responsibilities</h1>
-        <ul>
-          <li>
-            Collaborated in the development of a full-stack web application using <b>Node</b>, <b>React</b> JS, Bootstrap, and <b>MySQL</b> to improve data accuracy and usability
-          </li>
-          <li>
-            Directed the development of a cross-matching system designed to detect and resolve duplicate or inconsistent records across multiple data sources
-          </li>
-          <li>
-            Implemented intelligent matching logic and automated data validation routines to ensure data cleanliness, consistency, and integrity
-          </li>
-        </ul>
+      <div class="expContainerBody">
+        <div class="keyRespCard">
+          <h1>Key Responsibilities</h1>
+          <ul>
+            <li>
+              Led the development of the <b>Human Resource Information System</b> improving the hiring process, HR workflow efficiency, and reduce dependency on manual spreadsheet and email-based tracking
+            </li>
+            <li>
+              Developed web applications using <b>Alpine JS</b> and <b>Typescript</b> for front-end. <b>PHP</b> with <b>Eloquent ORM</b> for back-end, and <b>Mysql</b> for the Database
+            </li>
+            <li>
+              Ensure clean and maintainable through the integration of PHP Stan for static analysis, and improved documentation practices
+            </li>
+            <li>
+              Collaborated on the development rebuilding of the  <b>agency's website</b> and <b>content management system</b>, contributing to both front-end and back-end features to meet project deliverables and deadlines
+            </li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <label>Skills</label>
+      <div class="expContainerSkills">
+        <label>Technical Skills</label>
         <ul class="skillBadge">
           <li>Alpine JS</li>
           <li>PHP</li>
           <li>Eloquent</li>
+          <li>MySQL</li>
+        </ul>
+      </div>
+    </div>
+  `,
+  da: `
+    <div class="expContainer">
+      <div class="expContainerHeader">
+        <h1>Computer Programmer I</h1>
+      </div>
+      <div class="expContainerBody">
+        <div class="keyRespCard">
+          <h1>Key Responsibilities</h1>
+          <ul>
+            <li>
+              Collaborated in the development of a full-stack web application using <b>Node</b>, <b>React JS</b>, <b>Bootstrap</b>, and <b>MySQL</b> to improve data accuracy and usability
+            </li>
+            <li>
+              Directed the development of a cross-matching system designed to detect and resolve duplicate or inconsistent records across multiple data sources
+            </li>
+            <li>
+              Implemented intelligent matching logic and automated data validation routines to ensure data cleanliness, consistency, and integrity
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="expContainerSkills">
+        <label>Technical Skills</label>
+        <ul class="skillBadge">
+          <li>Node</li>
+          <li>React JS</li>
+          <li>Bootstrap</li>
           <li>MySQL</li>
         </ul>
       </div>
@@ -77,60 +87,70 @@ const experiences = {
   `,
   freelancing: `
     <div class="expContainer">
-      <h1>Full-stack Developer</h1>
-      <div class="keyRespCard">
-        <h1>Key Responsibilities</h1>
-        <ul>
-          <li>
-            Developed and maintained full stack web applications using the MERN stack: <b>MongoDB</b>, <b>Express JS</b>, <b>React JS</b> and <b>Node JS</b>. Utilized <b>Tailwind</b> for user-friendly designs with <b>DaisyUI</b> framework.
-          </li>
-          <li>
-            Automated processes for Project Tracking for an Engineering firm, Scheduler for a school, BudgetTracker in Mobile
-          </li>
-        </ul>
+      <div class="expContainerHeader">
+        <h1>Full-stack Developer</h1>
       </div>
-      <div>
-        <label>Skills</label>
+      <div class="expContainerBody">
+        <div class="keyRespCard">
+          <h1>Key Responsibilities</h1>
+          <ul>
+            <li>
+              Developed and maintained full stack web applications using the MERN stack: <b>MongoDB</b>, <b>Express JS</b>, <b>React JS</b>, and <b>Node JS</b>. Utilized <b>Tailwind</b> for user-friendly designs with <b>DaisyUI</b> framework.
+            </li>
+            <li>
+              Automated processes for Project Tracking for an Engineering firm, Scheduler for a school, and BudgetTracker in Mobile
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="expContainerSkills">
+        <label>Technical Skills</label>
         <ul class="skillBadge">
-          <li>Alpine JS</li>
-          <li>PHP</li>
-          <li>Eloquent</li>
-          <li>MySQL</li>
+          <li>MongoDB</li>
+          <li>Express JS</li>
+          <li>React JS</li>
+          <li>Node JS</li>
+          <li>Tailwind</li>
+          <li>DaisyUI</li>
         </ul>
       </div>
     </div>
   `,
   intern: `
     <div class="expContainer">
-      <h1>Full-stack Web Developer Intern</h1>
-      <div class="keyRespCard">
-        <h1>Key Responsibilities</h1>
-        <ul>
-          <li>
-            Developed and integrated a robust <b>authentication system</b> using a <b>microservice architecture</b>, ensuring secure and seamless communication across internal APIs
-          </li>
-          <li>
-            Designed <b>token-based authentication</b> workflows to support scalable, modular service interactions
-          </li>
-          <li>
-            Conducted regular code reviews to maintain high standards of code quality, performance, and adherence to best practices
-          </li>
-        </ul>
+      <div class="expContainerHeader">
+        <h1>Full-stack Web Developer Intern</h1>
       </div>
-      <div>
-        <label>Skills</label>
+      <div class="expContainerBody">
+        <div class="keyRespCard">
+          <h1>Key Responsibilities</h1>
+          <ul>
+            <li>
+              Developed and integrated a robust <b>authentication system</b> using a <b>microservice architecture</b>, ensuring secure and seamless communication across internal APIs
+            </li>
+            <li>
+              Designed <b>token-based authentication</b> workflows to support scalable, modular service interactions
+            </li>
+            <li>
+              Conducted regular code reviews to maintain high standards of code quality, performance, and adherence to best practices
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="expContainerSkills">
+        <label>Technical Skills</label>
         <ul class="skillBadge">
-          <li>Alpine JS</li>
-          <li>PHP</li>
-          <li>Eloquent</li>
-          <li>MySQL</li>
+          <li>Microservices</li>
+          <li>Token-based Auth</li>
+          <li>API Integration</li>
+          <li>Code Review</li>
         </ul>
       </div>
     </div>
   `,
 };
 
-//Set default experience to present
+//Set default experience to present experience
 const expContent = document.getElementById("expContent");
 expContent.innerHTML = experiences["gcg"];
 
