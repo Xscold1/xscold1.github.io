4174c2a1eb712d1a8b9c09c854ad9207f90cec7e
xscold1 <vashivan116@gmail.com>
2026-05-06 22:10:58 +0800

    Fix vite config for deployment in github pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,15 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+    build: {
+    rollupOptions: {
+      output: {
+        // Leave these exactly like this! 
+        // Vite will turn "[name]-[hash].js" into something like "index-82bc1a.js"
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  }
 })
